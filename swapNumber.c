8f45dadb0065019559a5068f176c854223e14b0a
kishor40 <121535444+kishor40@users.noreply.github.com>
2023-01-08 13:21:37 +0530

    initial commit

--- swapNumber.c
+++ swapNumber.c
@@ -1,14 +1,14 @@
     #include <stdio.h>
-    void kis(int *a, int *b);
+    void swap(int *a, int *b);
 
     int main()
     {
         int a = 2, b = 6;
-        kis(&a, &b);
+        swap(&a, &b);
         printf("%d,%d", a, b);
         return 0;
     }
-    void kis(int *a, int *b)
+    void swap(int *a, int *b)
     {
         int temp = *a;
         *a = *b;
